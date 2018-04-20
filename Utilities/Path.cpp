1115d71fcd35f6ff48b839cd4427ab1eb8de54cb
tkdals1049 <37647297+tkdals1049@users.noreply.github.com>
2018-04-20 21:21:16 +0900

    push_PathClass

---
+++ Utilities/Path.cpp
@@ -0,0 +1,158 @@
+#include "../stdafx.h"
+#include "Path.h"
+#include <string.h>
+#include "String.h"
+
+string Path::Combine(string path1, string path2)
+{
+	return path1 + path2;
+}
+
+wstring Path::Combine(wstring path1, wstring path2)
+{
+	return path1 + path2;
+}
+
+string Path::Combine(vector<string> paths)
+{
+	string temp = "";
+	for (string path : paths)
+		temp += path;
+
+	return temp;
+}
+
+wstring Path::Combine(vector<wstring> paths)
+{
+	wstring temp = L"";
+	for (wstring path : paths)
+		temp += path;
+
+	return temp;
+}
+
+string Path::GetDirectoryName(string path)
+{
+	size_t index = path.find_last_of('/');
+
+	return path.substr(0, index + 1);
+}
+
+wstring Path::GetDirectoryName(wstring path)
+{
+	size_t index = path.find_last_of('/');
+
+	return path.substr(0, index + 1);
+}
+
+string Path::GetExtension(string path)
+{
+	size_t index = path.find_last_of('.');
+
+	return path.substr(index + 1, path.length());;
+}
+
+wstring Path::GetExtension(wstring path)
+{
+	size_t index = path.find_last_of('.');
+
+	return path.substr(index + 1, path.length());;
+}
+
+string Path::GetFileName(string path)
+{
+	size_t index = path.find_last_of('/');
+
+	return path.substr(index + 1, path.length());
+}
+
+wstring Path::GetFileName(wstring path)
+{
+	size_t index = path.find_last_of('/');
+
+	return path.substr(index + 1, path.length());
+}
+
+string Path::GetFileNameWithoutExtension(string path)
+{
+	string fileName = GetFileName(path);
+
+	size_t index = fileName.find_last_of('.');
+	return fileName.substr(0, index);
+}
+
+wstring Path::GetFileNameWithoutExtension(wstring path)
+{
+	wstring fileName = GetFileName(path);
+
+	size_t index = fileName.find_last_of('.');
+	return fileName.substr(0, index);
+}
+
+
+const WCHAR* Path::ImageFilter = L"Image\0*.png;*.bmp;*.jpg";
+const WCHAR* Path::BinModelFilter = L"Binary Model\0*.model";
+const WCHAR* Path::FbxModelFilter = L"Fbx Model\0*.fbx;*.obj\0";
+const WCHAR* Path::ShaderFilter = L"HLSL file\0*.hlsl";
+
+void Path::OpenFileDialog(wstring file, const WCHAR* filter, wstring folder, function<void(wstring)> func, HWND hwnd)
+{
+	WCHAR name[255];
+	wcscpy_s(name, file.c_str());
+
+	wstring tempFolder = folder;
+	String::Replace(&tempFolder, L"/", L"\\");
+
+	OPENFILENAME ofn;
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = filter;
+	ofn.lpstrFile = name;
+	ofn.lpstrFileTitle = L"불러오기";
+	ofn.nMaxFile = 255;
+	ofn.lpstrInitialDir = tempFolder.c_str();
+	ofn.Flags = OFN_NOCHANGEDIR;
+
+	if (GetOpenFileName(&ofn) == TRUE)
+	{
+		if (func != NULL)
+		{
+			wstring loadName = name;
+			String::Replace(&loadName, L"\\", L"/");
+
+			func(loadName);
+		}
+	}
+}
+
+void Path::SaveFileDialog(wstring file, const WCHAR* filter, wstring folder, function<void(wstring)> func, HWND hwnd)
+{
+	WCHAR name[255];
+	wcscpy_s(name, file.c_str());
+
+	wstring tempFolder = folder;
+	String::Replace(&tempFolder, L"/", L"\\");
+
+	OPENFILENAME ofn;
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = filter;
+	ofn.lpstrFile = name;
+	ofn.lpstrFileTitle = L"저장하기";
+	ofn.nMaxFile = 255;
+	ofn.lpstrInitialDir = tempFolder.c_str();
+	ofn.Flags = OFN_NOCHANGEDIR;
+
+	if (GetSaveFileName(&ofn) == TRUE)
+	{
+		if (func != NULL)
+		{
+			wstring loadName = name;
+			String::Replace(&loadName, L"\\", L"/");
+
+			func(loadName);
+		}
+	}
+}
